90a13dc950843a3bb2d7b03b36fd45002820c336
Kris Regan <regan154@morrisville.edu>
2022-02-23 21:36:22 -0500

    Add Torque to Rotate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float flttorqueAmount = 1f;
+    Rigidbody2D rb2d;
+    void Start()
+    {
+       rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(flttorqueAmount);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-flttorqueAmount);
+        }
+    }
+}
