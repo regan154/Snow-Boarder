1693883b37047673fcfa293ba0419e2efe0e9a31
Kris Regan <regan154@morrisville.edu>
2022-03-01 13:20:54 -0500

    Using FindObjectOfType

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float flttorqueAmount = 1f;
+    [SerializeField] float fltBoostSpeed = 30f;
+    [SerializeField] float fltBaseSpeed = 20f;
+    
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = fltBoostSpeed; 
+        }
+        else
+        {
+            surfaceEffector2D.speed = fltBaseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(flttorqueAmount);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-flttorqueAmount);
         }
